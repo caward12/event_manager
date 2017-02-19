641037cfb41ba23a85a76b1ea13160046a90c9d2
caward12 <ward.colleen.a@gmail.com>
2017-02-19 15:48:02 -0700

    completed best times and days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,9 +3,14 @@ require 'sunlight/congress'
 require 'erb'
 require 'pry'
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+  @hours = []
+  @weekdays= []
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
+
 def clean_phone_number(phone_number)
   if phone_number.nil?
     "0000000000"
@@ -16,9 +21,54 @@ def clean_phone_number(phone_number)
   elsif phone_number.length == 10
     phone_number
   else
-    phone_number = "0000000000 "
+    phone_number = "0000000000"
   end
 end
+
+def find_hours_registered(register_date)
+  DateTime.strptime(register_date, '%m/%d/%Y %H:%M').hour
+end
+
+def find_popular_hours(hours)
+  popular_hours = hours.inject(Hash.new(0)) do |count, hour|
+    count[hour] +=1
+    count
+  end
+  popular_hours.sort_by {|k, v| v}.reverse.to_h
+end
+
+def find_weekdays(register_date)
+  #binding.pry
+  DateTime.strptime(register_date, '%m/%d/%Y %H:%M').wday
+end
+
+def find_popular_weekday(weekdays)
+  popular_weekday = weekdays.inject(Hash.new(0)) do |count, day|
+    count[day] +=1
+    count
+  end
+  popular_weekday.sort_by {|k, v| v}.reverse.to_h
+
+end
+
+def weekdays_to_word(weekday)
+  if weekday == 0
+    "sunday"
+  elsif  weekday == 1
+    "monday "
+  elsif weekday == 2
+    "tuesday"
+  elsif weekday == 3
+    "wednesday"
+  elsif weekday == 4
+    "thursday"
+  elsif weekday == 5
+    "friday"
+  else
+    "saturday"
+  end
+end
+
 
 def legislators_by_zipcode(zipcode)
    Sunlight::Congress::Legislator.by_zipcode(zipcode)
@@ -43,17 +93,25 @@ erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
-  name = row[:first_name]
+  register_hour = find_hours_registered(row[:regdate])
+  register_weekday = find_weekdays(row[:regdate])
+  @hours << register_hour
+  @weekdays << register_weekday
 
-  phone_number = clean_phone_number(row[:homephone])
+  # name = row[:first_name]
+  #
+  # phone_number = clean_phone_number(row[:homephone])
+  #
+  # puts phone_number
+  #
+  # zipcode = clean_zipcode(row[:zipcode])
+  #
+  # legislators = legislators_by_zipcode(zipcode)
+  #
+  # form_letter = erb_template.result(binding)
+  #
+  # save_thank_you_letters(id,form_letter)
 
-  puts phone_number
-  
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letters(id,form_letter)
 end
+puts "The most popular time is #{find_popular_hours(@hours).keys[0]}:00"
+puts "the most poular day is #{weekdays_to_word(find_popular_weekday(@weekdays).keys[0])}"
